e927cbb010c2fe32e744e2518aeb53d0380f2059
Suzana Milicev <suzana.milicev16@gmail.com>
2024-03-18 14:34:21 +0100

    Revert "Update"
    This reverts commit 82f0aaa1f1287f8e1ab88824cbe2666673a36b2a.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
+// function defaultTask(cb) {
+
+//     cb();
+//   }
+  
+//   exports.default = defaultTask
+
 'use strict';
-
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-
-function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-};
-
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-};
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+sass.compiler = require('node-sass');
+gulp.task('sass', function () {
+   return gulp.src('./sass/**/*.scss')
+   .pipe(concat('custom.scss'))
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./dist/'));
+});
