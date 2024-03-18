9be40d200fffc366b6d6a3df685d2db0783eec30
Suzana Milicev <suzana.milicev16@gmail.com>
2024-03-18 14:49:56 +0100

    1.dan

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
-// function defaultTask(cb) {
-
-//     cb();
-//   }
-  
-//   exports.default = defaultTask
-
 'use strict';
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-sass.compiler = require('node-sass');
-gulp.task('sass', function () {
-   return gulp.src('./sass/**/*.scss')
-   .pipe(concat('custom.scss'))
-   .pipe(sass().on('error', sass.logError))
-   .pipe(gulp.dest('./dist/'));
-});
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+};
+
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+};
