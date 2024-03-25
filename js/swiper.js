bc904322ece29330c4e32e7ceef1f0057a32e91d
Suzana Milicev <suzana.milicev16@gmail.com>
2024-03-25 11:11:48 +0100

    Respniziva - ispravke

--- js/swiper.js
+++ js/swiper.js
@@ -10,8 +10,8 @@ const swiper1 = new Swiper('.swiper1', {
 const swiper2 = new Swiper('.swiper2', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 5,
 
     navigation: {
         nextEl: '.swiper-button-next',
@@ -19,14 +19,17 @@ const swiper2 = new Swiper('.swiper2', {
     },
 
     breakpoints: {
-        400: {
-            slidesPerView: 1,
-        },
-        790: {
+        630: {
             slidesPerView: 2,
         },
-        1100: {
+        895: {
             slidesPerView: 3,
+        },
+        1150: {
+            slidesPerView: 4,
+        },
+        1350: {
+            slidesPerView: 5,
         }
     }
 });
